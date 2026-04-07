5722b98ee51230b968d15c48ff6229e3dfc85cdd
wpotato2210 <w.thian@hotmail.com>
2026-04-08 01:27:52 +1200

    Add deterministic GUI wave C execution and tests

--- gui/bench_app/__init__.py
+++ gui/bench_app/__init__.py
@@ -1,6 +1,7 @@
 from .controller import BenchAppController, ControllerState, RuntimeState
 from .wave_a import WaveAResult, execute_gui_wave_a
 from .wave_b import WaveBResult, execute_gui_wave_b
+from .wave_c import WaveCResult, execute_gui_wave_c
 
 __all__ = [
     "BenchAppController",
@@ -10,4 +11,6 @@ __all__ = [
     "execute_gui_wave_a",
     "WaveBResult",
     "execute_gui_wave_b",
+    "WaveCResult",
+    "execute_gui_wave_c",
 ]
