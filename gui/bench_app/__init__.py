d3f243123da2a4f71f6845728f1d5d74c048d16d
wpotato2210 <w.thian@hotmail.com>
2026-04-08 01:34:35 +1200

    Add GUI Wave D execution and tests

--- gui/bench_app/__init__.py
+++ gui/bench_app/__init__.py
@@ -2,6 +2,7 @@ from .controller import BenchAppController, ControllerState, RuntimeState
 from .wave_a import WaveAResult, execute_gui_wave_a
 from .wave_b import WaveBResult, execute_gui_wave_b
 from .wave_c import WaveCResult, execute_gui_wave_c
+from .wave_d import WaveDResult, execute_gui_wave_d
 
 __all__ = [
     "BenchAppController",
@@ -13,4 +14,6 @@ __all__ = [
     "execute_gui_wave_b",
     "WaveCResult",
     "execute_gui_wave_c",
+    "WaveDResult",
+    "execute_gui_wave_d",
 ]
